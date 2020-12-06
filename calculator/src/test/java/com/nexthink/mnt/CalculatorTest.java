376cce028e90dd4f4b85d6397d43ec52e5be06cd
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-06 15:45:38 +0100

    Implement addTestCase2(). Add @DisplayName to tests.

--- calculator/src/test/java/com/nexthink/mnt/CalculatorTest.java
+++ calculator/src/test/java/com/nexthink/mnt/CalculatorTest.java
@@ -2,6 +2,7 @@ package com.nexthink.mnt;
 
 import com.nexthink.mnt.math.Calculator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /*
@@ -11,11 +12,27 @@ public class CalculatorTest {
     Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("Add 2 positive numbers")
     void addTestCase1() {
         // * Setup
         int n1 = 10;
         int n2 = 20;
         int expectedResult = 30;
+
+        // * Act
+        int actualResult = calculator.add(n1, n2);
+
+        // * Assert
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Add 2 negative numbers")
+    void addTestCase2() {
+        // * Setup
+        int n1 = -10;
+        int n2 = -20;
+        int expectedResult = -30;
 
         // * Act
         int actualResult = calculator.add(n1, n2);
