5d5fc74bfcfabc6d76aa9d268afbb1a7160f8161
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-06 11:23:25 +0100

    Added math package written by @author - GreenLearner to use as a ground for tests.

---
+++ calculator/src/main/java/com/nexthink/mnt/math/TooLargeNumbers.java
@@ -0,0 +1,10 @@
+package com.nexthink.mnt.math;
+
+/**
+ * @author - GreenLearner(https://www.youtube.com/c/greenlearner)
+ */
+public class TooLargeNumbers extends RuntimeException {
+    public TooLargeNumbers(String message) {
+        super(message);
+    }
+}
