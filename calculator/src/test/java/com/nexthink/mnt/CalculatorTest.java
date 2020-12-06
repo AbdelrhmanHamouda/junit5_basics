04afe4e95b85d6aeeb0aec1504836fa563411aa3
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-06 16:02:26 +0100

    Handle expected exceptions! Implement addTestCase3(). Implement addTestCase4().

--- calculator/src/test/java/com/nexthink/mnt/CalculatorTest.java
+++ calculator/src/test/java/com/nexthink/mnt/CalculatorTest.java
@@ -1,9 +1,13 @@
 package com.nexthink.mnt;
 
 import com.nexthink.mnt.math.Calculator;
+import com.nexthink.mnt.math.TooLargeNumbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /*
  * @author - Abdelrhman Hamouda
@@ -39,5 +43,41 @@ public class CalculatorTest {
 
         // * Assert
         Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test for a negative number exceptions")
+    void addTestCase3() {
+
+        // * Setup
+        int n1 = -101;
+        int n2 = -200;
+        String expectedMessage = "Numbers less than 100 are not allowed";
+
+        // * Act
+        // Here we are capturing the exception by first passing the expected exception to get
+        // and then we pass the function/method that is expected to cause the exception.
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> calculator.add(n1, n2));
+
+        // * Assert
+        assertEquals(expectedMessage, exception.getMessage());
+
+    }
+
+    @Test
+    @DisplayName("Test for numbers bigger than 50000")
+    void addTestCase4() {
+
+        // * Setup
+        int number1 = 500001;
+        int number2 = 100;
+        String expectedMessage = "Negative Numbers not allowed";
+
+        // * Act
+        TooLargeNumbers exception = assertThrows(TooLargeNumbers.class, () -> calculator.add(number1, number2));
+
+        // * Assert
+        assertEquals(expectedMessage, exception.getMessage());
+
     }
 }
