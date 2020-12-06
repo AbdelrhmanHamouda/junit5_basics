1ae0197a51b290e99328f0f31784a17f1f59e054
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-06 14:12:17 +0100

    Implement addTestCase1()

--- calculator/src/test/java/com/nexthink/mnt/CalculatorTest.java
+++ calculator/src/test/java/com/nexthink/mnt/CalculatorTest.java
@@ -1,2 +1,26 @@
-package com.nexthink.mnt;public class CalculatorTest {
+package com.nexthink.mnt;
+
+import com.nexthink.mnt.math.Calculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/*
+ * @author - Abdelrhman Hamouda
+ */
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    void addTestCase1() {
+        // * Setup
+        int n1 = 10;
+        int n2 = 20;
+        int expectedResult = 30;
+
+        // * Act
+        int actualResult = calculator.add(n1, n2);
+
+        // * Assert
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
 }
