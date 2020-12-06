abd3816b6ea2c7a70a1eb86f7e80fbd381fdc6eb
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-06 09:46:24 +0100

    Init commit for test class.

--- calculator/src/test/java/com/nexthink/mnt/DemoTest.java
+++ calculator/src/test/java/com/nexthink/mnt/DemoTest.java
@@ -1,4 +1,26 @@
+package com.nexthink.mnt;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DemoTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void howToProtectFromCOVID() {
+    }
+
+    @Test
+    void currentlyInfectedPeople() {
+    }
 }
