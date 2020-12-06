774f55ca872a237c0623358ee330381026485873
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-06 09:46:05 +0100

    Create a very basic source code to use as a target for tests.

---
+++ calculator/src/main/java/com/nexthink/mnt/Demo.java
@@ -0,0 +1,17 @@
+package com.nexthink.mnt;
+
+/*
+ * @author - Abdelrhman Hamouda
+ */
+public class Demo {
+
+    public String howToProtectFromCOVID() {
+        return "Stay home & stay safe!";
+    }
+
+    public int currentlyInfectedPeople(int add) {
+        int previousNumber = 13000;
+        return previousNumber + add;
+    }
+
+}
